50ff15377706ac6564cba4a0c2d65c2fe64f31c4
Sammi Haj Hassine <sammi.hajhassine@gmail.com>
2016-11-02 20:10:12 +0100

    Added welcome component

--- app/assets/js/components/App.js
+++ app/assets/js/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Service from "./Services";
+import Welcome from "./Welcome";
 
 export default function () {
   return (
     <div>
       <Header />
+      <Welcome />
       <Service />
     </div>
   );
--- app/assets/js/components/Welcome.js
+++ app/assets/js/components/Welcome.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export default function () {
   return (
-    <div></div>
+    <div className="welcome-part">
+      <h1 className="welcome-part__headline">Välkommen till <span className="main-color">Alphadev</span></h1>
+      <p className="welcome-part__paragraph">Alphadev är ett konsultbolag med mycket erfarna specialister inom systemutveckling.
+        Vi är inriktade på, men inte begränsade till arkitektur och agil utveckling på
+        Java- och .NET-plattformen, samt frontendutveckling.</p>
+    </div>
   );
 }
