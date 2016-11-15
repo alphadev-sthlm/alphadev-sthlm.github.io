0a5f65e58a8274b6017b17de1a245464dbe564b1
Leon Hennings <leon.hennings@assessio.se>
2016-11-15 21:30:52 +0100

    Style contact-part, somewhat

--- app/assets/js/components/Contact.js
+++ app/assets/js/components/Contact.js
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function () {
   return (
-    <contact className="contact-part">
-      <div className="contact-part__join">
-        blaj
+    <div className="contact-part">
+      <div className="contact-part__content">
+        <div className="contact-part__column"><p>Join us</p></div>
+        <div className="contact-part__column"> <p>Contact</p></div>
+        <div className="contact-part__column"> <p>More</p></div>
       </div>
-    </contact>
+    </div>
   );
 }
