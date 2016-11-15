a8e24751751568c696262bba6188efdaa0adf0d8
Olle Kallström <olle.kallstrom@gmail.com>
2016-11-15 19:42:09 +0100

    Added more consultants

--- app/assets/js/components/Consultants.js
+++ app/assets/js/components/Consultants.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { CONSULTANTS } from "../constants";
 
+function shuffleArray(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
 function Consultants() {
 
-  return (
+  const shuffledConsultants = shuffleArray(CONSULTANTS);
 
+  return (
     <div className="consultants-part">
-      {CONSULTANTS.map((consultant, index) => {
+      {shuffledConsultants.map((consultant, index) => {
         return (
             <div key={index} className="consultants-part__consultant">
               <div className="consultants-part__consultant-wrapper">
@@ -18,7 +30,7 @@ function Consultants() {
                 <a href={consultant.linkedin}> <img width="30" height="30" src={"public/assets/img/linkedin.svg"} /> </a>
               </div>
             </div>
-          );
+        );
       })}
     </div>
   );
--- app/assets/js/constants.js
+++ app/assets/js/constants.js
@@ -23,5 +23,60 @@ export const CONSULTANTS = [
     linkedin: "",
     title: "Inte utvecklare",
     image: "public/assets/img/consultants/jon_sml.png"
+  },
+  {
+    name: "Jonas Montonen",
+    linkedin: "",
+    title: "Utvecklare",
+    image: "public/assets/img/consultants/jonas_sml.jpg"
+  },
+  {
+    name: "Peter Österdahl",
+    linkedin: "",
+    title: "Utvecklare",
+    image: "public/assets/img/consultants/peter_sml.jpg"
+  },
+  {
+    name: "Daniel Lundström",
+    linkedin: "",
+    title: "Utvecklare",
+    image: "public/assets/img/consultants/daniel_sml.jpg"
+  },
+  {
+    name: "Daniel Hamner",
+    linkedin: "",
+    title: "Utvecklare",
+    image: "public/assets/img/consultants/hamner_sml.png"
+  },
+  {
+    name: "Fredrik Bergström",
+    linkedin: "",
+    title: "Utvecklare",
+    image: "public/assets/img/consultants/fredrik_sml.jpg"
+  },
+  {
+    name: "Eduardo Castaneda",
+    linkedin: "",
+    title: "Utvecklare",
+    image: "public/assets/img/consultants/eddo_sml.png"
+  },
+  {
+    name: "Johan Windahl",
+    linkedin: "",
+    title: "Utvecklare",
+    image: "public/assets/img/consultants/johan_w_sml.jpg"
+  },
+  {
+    name: "Johan Lindgren",
+    linkedin: "",
+    title: "Utvecklare",
+    image: "public/assets/img/consultants/johan_l_sml.jpg"
+  },
+  {
+    name: "Tommy Lindberg",
+    linkedin: "",
+    title: "Utvecklare",
+    image: "public/assets/img/consultants/tommy_sml.jpg"
   }
+
 ];
