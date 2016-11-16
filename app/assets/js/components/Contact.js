deb599f30cd64b2d4db424a12392b1dedf6072a4
Olle Kallström <olle.kallstrom@gmail.com>
2016-11-16 10:34:05 +0100

    Style contact part

--- app/assets/js/components/Contact.js
+++ app/assets/js/components/Contact.js
@@ -4,9 +4,21 @@ export default function () {
   return (
     <div className="contact-part">
       <div className="contact-part__content">
-        <div className="contact-part__column"><p>Join us</p></div>
-        <div className="contact-part__column"> <p>Contact</p></div>
-        <div className="contact-part__column"> <p>More</p></div>
+        <div className="contact-part__column">
+          <h3 className="contact-part__headline">Join us now</h3>
+          <p className="contact-part__text">Lorem Ipsum is simply dummy text of the printing and type setting industry.</p>
+        </div>
+        <div className="contact-part__column">
+          <h3 className="contact-part__headline">Contact us</h3>
+          <p className="contact-part__text">Alphadev AB <br/> Vasagatan 15<br/> 111 20 Stockholm<br/>+46 (0) 702 - 96 00 05</p>
+        </div>
+        <div className="contact-part__column">
+          <h3 className="contact-part__headline">Quick links</h3>
+          <p className="contact-part__text">Lorem Ipsum is simply dummy text of the printing and type setting industry.</p>
+        </div>
+        <div className="contact-part__column">
+          <h3 className="contact-part__headline">Social</h3>
+        </div>
       </div>
     </div>
   );
