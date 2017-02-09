b7a1eb12e0d19ce624f908ec144b160b317224eb
Eduardo Castaneda <eduardo.castaneda25@gmail.com>
2017-02-09 19:52:47 +0100

    Added intro part to page

--- app/assets/js/components/App.js
+++ app/assets/js/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Intro from "./Intro";
 import Service from "./Services";
 import Welcome from "./Welcome";
 import Consultants from "./Consultants";
@@ -9,6 +10,7 @@ export default function () {
   return (
     <div>
       <Header />
+      <Intro />
       <Welcome />
       <Service />
       <Consultants />
---
+++ app/assets/js/components/Intro.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div className="intro">
+      <div className="into__title">Alphadev</div>
+    </div>
+  );
+}
