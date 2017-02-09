8591dadc15117c24b639aff0d2bdf5e07efaa306
Leon Hennings <leon.hennings@assessio.se>
2017-02-09 21:19:35 +0100

    Half-assed stickyheader

--- app/assets/js/components/Header.js
+++ app/assets/js/components/Header.js
@@ -3,9 +3,29 @@ import React from "react";
 export default function () {
   return (
     <header className="main-header">
-      <li>
-        <ul></ul>
-      </li>
+      <div className="main-header__wrapper">
+        <img className="main-header__logo" src="public/assets/img/header_logo.svg"/>
+        <ul className="main-header__list">
+          <li className="main-header__item">
+            <a className="main-header__link" href="index.html">Home</a>
+          </li>
+          <li className="main-header__item">
+            <a className="main-header__link" href="#">About Us</a>
+          </li>
+          <li className="main-header__item">
+            <a className="main-header__link" href="#">Our Services</a>
+          </li>
+          <li className="main-header__item">
+            <a className="main-header__link" href="#">Consultants</a>
+          </li>
+          <li className="main-header__item">
+            <a className="main-header__link" href="#">Blog</a>
+          </li>
+          <li className="main-header__item">
+            <a className="main-header__link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
