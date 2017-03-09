7fc231f9aa92e589ad5c9e74a81479d65d458b29
Eduardo Castaneda <eduardo.castaneda25@gmail.com>
2017-03-09 21:27:41 +0100

    Pushing public catalog

--- app/assets/js/components/Intro.js
+++ app/assets/js/components/Intro.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 export default () => {
+  const subtitle = 'Architecture - Development - Agile methodology';
+
   return (
     <div className="intro-part">
       <img src="public/assets/img/logos/banner_logo.png" className="intro-part__banner" />
+      <span className="intro-part__subtitle">{subtitle}</span>
     </div>
   );
 }
