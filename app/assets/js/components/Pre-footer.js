65b610d55e64c64b8932535f6bace3e1ff7703c3
jonnee-ad <jon.neergaard@alphadev.se>
2017-09-21 17:50:55 +0200

    Minor text change

--- app/assets/js/components/Pre-footer.js
+++ app/assets/js/components/Pre-footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function () {
   const p1 = "Är du intresserad av våra tjänster och vill veta lite mer?";
-  const p2 = "Eller en vass utvecklare och funderar på en ny hemvist?";
+  const p2 = "Eller är du en vass utvecklare som funderar på ny hemvist?";
   const p3 = "Jon Neergaard";
   const p4 = "CEO & Sales";
 
