2912134095e228c4d7ba336410726b16712dd620
Leon Hennings <leon.hennings@gmail.com>
2018-01-15 22:14:02 +0100

    Test relative linking with rawgit

--- app/assets/js/actions/appData.js
+++ app/assets/js/actions/appData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function loadData() {
   return (dispatch) => {
-    axios.get(`/data.json?v=${ new Date().getTime()}`)
+    axios.get(`./data.json?v=${ new Date().getTime()}`)
       .then((response) => {
         dispatch({
           type: 'SET_APPLICATION_DATA',
