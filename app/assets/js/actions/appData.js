f0ce531df5d71d7f789a6285270efe3efcdc7712
Leon Hennings <leon.hennings@gmail.com>
2018-01-15 22:05:09 +0100

    Relative link to data.json

--- app/assets/js/actions/appData.js
+++ app/assets/js/actions/appData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function loadData() {
   return (dispatch) => {
-    axios.get('https://raw.githubusercontent.com/alphadev-sthlm/alphadev-sthlm.github.io/master/data.json?v=' + new Date().getTime())
+    axios.get(`/data.json?v=${ new Date().getTime()}`)
       .then((response) => {
         dispatch({
           type: "SET_APPLICATION_DATA",
