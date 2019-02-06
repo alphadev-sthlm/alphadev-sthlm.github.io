df9f278f459906e787b2d3c4d7a7fb9c5e66db40
Johan Windahl <johan.windahl@evokegaming.com>
2019-02-06 15:40:17 +0100

    Added production to Node_Env

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,9 @@ const loaders = [{
 
 const plugins = [
   new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+  }),
   new ExtractText('styles.css', {
     allChunks: true
   }),
